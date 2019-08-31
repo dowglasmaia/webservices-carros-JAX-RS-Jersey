c25f1eb13dddd71705ed51e1e69b34f4e8189381
Dowglas Maia <dowglasmaia@gmail.com>
2019-08-31 10:06:34 -0300

    Creating connection with JDBC

---
+++ src/br/com/maia/domain/BaseDAO.java
@@ -0,0 +1,36 @@
+package br.com.maia.domain;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BaseDAO {
+
+	public BaseDAO() {
+		// class JDBC
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected Connection connection() throws SQLException {
+		// URL de conexão
+		String url = "jdbc:mysql://localhost/carros";
+
+		// conect
+		Connection conn = DriverManager.getConnection(url, "root", "2522");
+		return conn;
+
+	}
+	/*
+	//testando conexão
+	 public static void main(String[] args) throws SQLException {
+		BaseDAO db = new BaseDAO();
+		
+		Connection cn = db.connection();
+		System.out.println(cn);
+	}
+*/
+}
